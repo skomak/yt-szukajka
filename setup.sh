52a79181838a790a35396768c7a47b2b6418beea
skomak <skomak@users.noreply.github.com>
2024-01-09 19:16:17 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,3 @@ python -m venv venv
 source venv/bin/activate
 pip install faster_whisper yt_dlp torch ipython nvidia-cublas-cu11 # faster_whisper
 pip install transformers accelerate sentencepiece bitsandbytes # trurl
-pip install tensorflow # misc
